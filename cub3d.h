73105504c2acb27a0552a25a3c3965403ab5538a
fesper-s <fafaeesfabricio@gmail.com>
2023-03-10 08:36:43 -0300

    adding include mlx on cub3d.h

--- cub3d.h
+++ cub3d.h
@@ -6,12 +6,18 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 08:13:26 by fesper-s          #+#    #+#             */
-/*   Updated: 2023/03/10 08:18:45 by fesper-s         ###   ########.fr       */
+/*   Updated: 2023/03/10 08:36:25 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+
+# ifdef __linux__
+#  include "mlx_linux/mlx.h"
+# else
+#  include "mlx/mlx.h"
+# endif
 
 # include <stdio.h>
 # include <fcntl.h>
