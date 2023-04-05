6360fc2707cd501fd624b571a26f5cb67ee3be7d
fesper-s <fesper-s@student.42.rio>
2023-04-05 10:32:14 -0300

    removing leaks from texture functions

--- src/texture_utils.c
+++ src/texture_utils.c
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 13:22:18 by fesper-s          #+#    #+#             */
-/*   Updated: 2023/04/04 09:06:35 by fesper-s         ###   ########.fr       */
+/*   Updated: 2023/04/05 10:31:03 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ int	rgb_to_hex(char *rgb)
 	i = -1;
 	while (++i < 3)
 		rgb_value[i] = ft_atoi(buffer[i]);
+	free_charpp(buffer);
 	hex_value = (rgb_value[0] << 16) | (rgb_value[1] << 8) | rgb_value[2];
 	free(rgb_value);
 	return (hex_value);
